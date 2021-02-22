f2a975d9028d9ea8655258a34d9d8a7dfee189f5
Andrews Peter <51387040+AndrewstheBuilder@users.noreply.github.com>
2021-02-22 08:39:39 -0500

    Add files via upload
    Add or remove ACLS on Folders. Enable all privilege access tokens.

---
+++ Add-Acl.ps1
@@ -0,0 +1,101 @@
+﻿# IN TEST MODE check $rootPath
+# TASK -> ADD 'Share-Global-All-Full Ctrl' to all folders in \\smb01.unfcsd.unf.edu\global
+# 
+# 
+#
+#
+
+CLEAR
+$Error.Clear()
+
+# Set properties
+$identity = "Share-Global-All-Full Ctrl"
+$fileSystemRights = "FullControl"
+$type = "Allow"
+
+# Create new rule
+$fileSystemAccessRuleArgumentList = $identity, $fileSystemRights, $type
+$fileSystemAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $fileSystemAccessRuleArgumentList
+
+# Declare other variables for script
+$rootPath = 'C:\Users\AndrewsP\Documents\Add-FullControl\Scans'
+$subPaths = Get-ChildItem -LiteralPath $RootPath -Directory 
+$subFolderList = $subPaths.FullName
+$folderAddList = @() # folders where $identity was added
+$xml = @() # previous ACL before $identity is added to ACL
+$regex = "*" + $identity + "*" # to be used with Where command
+
+ForEach( $subFolder in $subFolderList ) {
+    CLEAR
+    
+    # See if first level subdirectories have user($identity) with specified access($fileSystemRights)
+    $acl = Get-Acl -LiteralPath $subFolder
+    $subFolder
+    $contains = $acl.Access | Where{$_.IdentityReference -like $regex}
+    if( ($contains.count -eq 0) -or ($contains.FileSystemRights -ne $fileSystemRights) ) {
+        $xml += Get-Acl -LiteralPath $subFolder # add backup of previous acl to .xml
+        $item = New-Object –TypeName PSObject
+            $item | Add-Member –MemberType NoteProperty –Name "Folder Path" -Value $subFolder
+            $item | Add-Member –MemberType NoteProperty –Name "Add User" -Value $identity
+        $folderAddList += $item
+    }
+
+    # See if all subdirectories in first level folder have user($identity) with specified access($fileSystemRights)
+    Get-ChildItem -LiteralPath $subFolder -Recurse -Directory | %{
+	    $folder = $_.FullName
+        $acl = Get-Acl -LiteralPath $folder
+        $folder
+        $contains = $acl.Access | Where{$_.IdentityReference -like $regex}
+
+        # if a folder has at least one inherited ACL then inheritance is enabled
+        $isInherited = $acl.Access | where{$_.IsInherited -eq $true}
+        if( $isInherited.Count -gt 0 ) { 
+        
+        } elseif( ($contains.count -eq 0) -or ($contains.FileSystemRights -ne $fileSystemRights) ) {
+            $xml += Get-Acl -LiteralPath $folder # add backup of previous acl to .xml
+            $item = New-Object –TypeName PSObject
+                $item | Add-Member –MemberType NoteProperty –Name "Folder Path" -Value $folder
+                $item | Add-Member –MemberType NoteProperty –Name "Add User" -Value $identity
+            $folderAddList += $item
+        }
+    }
+
+    # Append to .csv 
+    $folderAddList | Export-CSV -LiteralPath .\global-NeedsFullCtrl.csv -NoTypeInformation -Append
+    $folderAddlist = @()
+
+    # backup of $acl for each folder before user($identity) with FileSystemRights($fileSystemRights) were added 
+    $filePath = $subFolder.Replace('\', '.')
+	$xml | Export-Clixml .\$filePath.xml 
+    $xml = @()
+
+    $Error | Export-Csv -LiteralPath .\globalErrors-NeedsFullCtrl.csv -NoTypeInformation -Append
+    $Error.Clear()
+}
+
+
+
+<# Add User
+CLEAR
+$importedCsv = Import-Csv .\global-NeedsFullCtrl.csv
+foreach( $line in $importedCsv ) {
+        $folderPath = $line.'Folder Path'
+        $acl = Get-Acl -LiteralPath $folderPath
+        $workAround = $false
+
+        # this is a workaround for when filenames have special characters that the .csv format couldn't interpret
+        if( $acl -eq $null ) { 
+            $acl = Get-Acl -Path $folderPath 
+            $workAround = $true
+        }
+
+        $acl.SetAccessRule($fileSystemAccessRule)
+        if( $workAround -eq $false ) { 
+            Set-Acl -LiteralPath $folderPath -AclObject $acl
+        } else {
+            Set-Acl -Path $folderPath -AclObject $acl
+        }
+        Write-Host -ForegroundColor Red -BackgroundColor Blue 'Added' $identity 'to' $folderPath
+        $Error | Export-CSV -LiteralPath .\addUserGlobalErrors.csv -NoTypeInformation -Append
+        $Error.Clear()
+}#>
---
+++ Delete-Acl.ps1
@@ -0,0 +1,98 @@
+﻿# Run as admin
+# Make sure correct token privileges are enabled check enabled tokens. Check by running command 'whoami /priv'
+# To enable all token privilieges run my script Enable-AllPriv.ps1
+# Check current directory -> make sure the .csv files that are appended to in this script are not there
+#
+# \\smb01.unfcsd.unf.edu\global
+#
+#
+
+CLEAR
+$Error.Clear()
+$RootPath = '\\smb01.unfcsd.unf.edu\global'
+$FolderPurgeList = @()
+$GlobalXML = @() # array of XMl objects -> XML objects are backups of ACL before some ACLs are removed
+
+# get first-level subdirectories of $RootPath
+$SubPaths = Get-ChildItem -LiteralPath $RootPath -Directory 
+$SubFolderList = $SubPaths.FullName
+
+ForEach( $SubFolder in $SubFolderList ) {
+    CLEAR
+
+    # Check $ACL on first level $Subfolders on $RootPath
+    $SubFolder 
+    $ACL = Get-Acl -LiteralPath $SubFolder
+    $RemoveUser = $ACL.Access | where {($_.IdentityReference -like "*CREATOR OWNER*" -or $_.IdentityReference -like "S-*-*-*-*")}
+    if( $RemoveUser.count -gt 0 ) { $GlobalXML += Get-Acl -LiteralPath $SubFolder }
+    ForEach ($UserName in $RemoveUser ) {
+        $itemHasPerm = New-Object –TypeName PSObject
+            $itemHasPerm | Add-Member –MemberType NoteProperty –Name "Target User" -Value $UserName.IdentityReference
+            $itemHasPerm | Add-Member –MemberType NoteProperty –Name "Folder Path" -Value $SubFolder
+        $FolderPurgeList += $itemHasPerm
+    } 
+
+    # Check $ACL on $AllSubFolders inside a first-level subdirectory of the $RootPath
+    Get-ChildItem -LiteralPath $SubFolder -Recurse -Directory | %{
+            $Folder = $_.FullName
+            $Folder
+            $ACL = Get-Acl -LiteralPath $Folder
+            $RemoveUser = $ACL.Access | where {($_.IdentityReference -like "*CREATOR OWNER*" -or $_.IdentityReference -like "S-*-*-*-*")}
+            if( $RemoveUser.count -gt 0 ) { $GlobalXML += Get-Acl -LiteralPath $Folder }
+            ForEach ($UserName in $RemoveUser ) {
+                if( $UserName.IsInherited -eq $true ){ continue }
+                $itemHasPerm = New-Object –TypeName PSObject
+                    $itemHasPerm | Add-Member –MemberType NoteProperty –Name "Target User" -Value $UserName.IdentityReference
+                    $itemHasPerm | Add-Member –MemberType NoteProperty –Name "Folder Path" -Value $Folder
+                $FolderPurgeList += $itemHasPerm
+            }
+    }
+
+    # Append Errors and PurgeList to .csv files
+    $FolderPurgeList | Export-CSV -LiteralPath .\global2.csv -NoTypeInformation -Append
+    $FolderPurgeList = @()
+    $filePath = $SubFolder.Replace('\', '.')
+    if( $GlobalXML -ne $null ) {
+        $GlobalXML | Export-Clixml .\$filePath.xml
+        $GlobalXML = @()
+    }
+    $Error | Export-CSV -LiteralPath .\globalErrors2.csv -NoTypeInformation -Append
+    $Error.Clear()
+}
+
+
+
+#remove users
+CLEAR
+$importedCsv = Import-Csv .\global2.csv
+foreach($line in $importedCsv) { 
+     $folderPath = $line.'Folder Path'
+     $removeUser = $line.'Target User'
+     $acl = Get-Acl -LiteralPath $folderPath
+     $workAround = $false
+
+     # this is a workaround for when filenames have special characters because the .csv doesn't interpret that
+     if( $acl -eq $null ) { 
+        $acl = Get-Acl -Path $folderPath 
+        $workAround = $true
+     }
+
+     $regex = $removeUser + "*"
+     $removeAcl = $acl.Access | where {$_.IdentityReference -like $regex}
+     if( $removeAcl -eq $null ) { 
+        Write-Host -ForegroundColor Magenta -BackgroundColor Black 'Warning: Acl' $removeUser 'not found in' $folderPath 
+        #break # remove this if you don't want the script to break on this condition
+     } 
+     foreach( $user in $removeAcl ) {
+        $acl.RemoveAccessRule( $user )
+     }
+     if( $workAround -eq $false ) { 
+        Set-Acl -LiteralPath $folderPath -AclObject $acl -ErrorAction Stop # applies removed acl to $folderPath
+     } else {
+        Set-Acl -Path $folderPath -AclObject $acl -ErrorAction Stop # applies removed acl to $folderPath
+     }
+     Write-Host -ForegroundColor Red -BackgroundColor Blue 'Removed user' $removeUser 'in' $folderPath
+     $Error | Export-CSV -LiteralPath .\removeUserGlobalErrors2.csv -NoTypeInformation -Append
+     $Error.Clear()
+}
+
