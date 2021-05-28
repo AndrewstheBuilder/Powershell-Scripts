c5480f50ec4ae6ae284af29cef1c13edb39e878b
Andrews Peter <51387040+AndrewstheBuilder@users.noreply.github.com>
2021-05-28 11:10:59 -0400

    Mouse manipulators

---
+++ Mouse-Move.ps1
@@ -0,0 +1,8 @@
+﻿while( $true ) {
+    $Pos = [System.Windows.Forms.Cursor]::Position
+    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X) - 100), $Pos.Y)
+    Start-Sleep -Seconds 10
+    $Pos = [System.Windows.Forms.Cursor]::Position
+    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X) + 100), $Pos.Y)
+    Start-Sleep -Seconds 10
+}
